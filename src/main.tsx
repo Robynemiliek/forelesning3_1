406200e95c1bda1621444c54d4cb23e2e8ddd8b2
robynemiliek <rokr012@student.kristiania.no>
2026-01-30 13:50:51 +0100

    a very very basic OpenLayers map

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import { createRoot } from "react-dom/client";
 import { Application } from "./components/app/application.js";
 
-createRoot(document.getElementById("root")!).render(Application());
+createRoot(document.getElementById("root")!).render(<Application />);
